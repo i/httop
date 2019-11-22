b25984d6516fcb6414b7369065e7fd64272fd25b
Ian Lozinski <ian.lozinski@gmail.com>
2019-11-22 09:52:45 -0500

    wip

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-	"net/url"
 	"runtime"
+	"sort"
 	"strings"
 	"time"
 
@@ -66,8 +66,14 @@ type Sniffer struct {
 	ReportInterval time.Duration
 
 	roundTrips    chan roundTripInfo
-	address       []string
+	stats         map[string][]frame
 	stopReporting chan struct{}
+}
+
+type frame struct {
+	ts   time.Time
+	up   int
+	down int
 }
 
 type roundTripInfo struct {
@@ -97,6 +103,27 @@ func processRequests(r io.Reader, ch chan *http.Request) {
 	}
 }
 
+// todo remove
+func handleUnexpectedResponse(r io.Reader) {
+	// log.Println("unexpected response!")
+	// log.Println("just returning")
+	return
+	buf := bufio.NewReader(r)
+	for {
+		log.Println("reading unexpected response...")
+		bb, err := ioutil.ReadAll(buf)
+		log.Println("...read it")
+		if err == io.EOF || (err == nil && len(bb) == 0) {
+			log.Println("empty unexpected response. exiting")
+			return
+		}
+		if err != nil {
+			log.Fatalf("Error reading http response: %v", err)
+		}
+		log.Printf("Unexpected response: %s", string(bb))
+	}
+}
+
 func processResponses(r io.Reader, ch chan *http.Request, roundTrips chan<- roundTripInfo) error {
 	buf := bufio.NewReader(r)
 	for {
@@ -107,13 +134,17 @@ func processResponses(r io.Reader, ch chan *http.Request, roundTrips chan<- roun
 		if err != nil {
 			log.Fatalf("Error reading http response: %v", err)
 		}
+
+		var cw countWriter
 		request := <-ch
-		_ = request
+		if err := request.Write(&cw); err != nil {
+			panic(err)
+		}
 		roundTrips <- roundTripInfo{
 			host:         request.Host,
 			path:         request.URL.String(),
-			requestSize:  4,
-			responseSize: 20,
+			requestSize:  int(cw),
+			responseSize: len(bb),
 		}
 	}
 }
@@ -146,7 +177,8 @@ func (s *httpStreamFactory) New(netFlow, tcpFlow gopacket.Flow) tcpassembly.Stre
 		hostport := fmt.Sprintf("%s:%s", netFlow.Src().String(), tcpFlow.Dst().String())
 		ch, ok := s.pending[hostport]
 		if !ok {
-			log.Fatal("no corresponding request for this response")
+			handleUnexpectedResponse(&r)
+			return &r
 		}
 		go processResponses(&r, ch, s.roundTrips)
 		return &r
@@ -163,6 +195,7 @@ func (s *Sniffer) Start() error {
 
 	handle.SetBPFFilter("tcp port 80") // TODO make this configurable
 	s.roundTrips = make(chan roundTripInfo, 1)
+	s.stats = make(map[string][]frame)
 	streamFactory := &httpStreamFactory{
 		pending:    make(map[string]chan *http.Request),
 		roundTrips: s.roundTrips,
@@ -193,9 +226,16 @@ func (s *Sniffer) Start() error {
 	return nil
 }
 
+// not threadsafe
 func (s *Sniffer) processRoundTrips() {
 	for rt := range s.roundTrips {
-		fmt.Println("round trip info:", rt)
+		section := sectionFromHostPath(rt.host, rt.path)
+		s.stats[section] = append(s.stats[section], frame{
+			ts:   time.Now(),
+			up:   rt.requestSize,
+			down: rt.responseSize,
+		})
+		s.ShowDisplay()
 	}
 }
 
@@ -205,6 +245,7 @@ func (s *Sniffer) displayLoop() {
 	for {
 		select {
 		case <-s.stopReporting:
+			t.Stop()
 			return
 		case <-t.C:
 			s.ShowDisplay()
@@ -213,19 +254,74 @@ func (s *Sniffer) displayLoop() {
 }
 
 func (s *Sniffer) ShowDisplay() {
-	fmt.Println("stats: nil")
+	now := time.Now()
+	showTS := now.Add(-time.Second * 5)
+	alertTS := now.Add(-time.Second * 10) // TODO make this 2 minutes or configurable
+
+	var rows []displayRow
+
+	for section, frames := range s.stats {
+		if len(frames) == 0 {
+			// TODO make this threadsafe
+			delete(s.stats, section)
+			continue
+		}
+
+		var displayHits, displayUp, displayDown, displayTotal, alertHits, alertUp, alertDown, alertTotal int
+		for i := range frames {
+			// this is just iterating backwards
+			i = len(frames) - 1 - i
+			f := frames[i]
+
+			if f.ts.Before(alertTS) {
+				// remove this and all the frames preceding it
+				s.stats[section] = frames[i+1:]
+				break
+			}
+
+			alertHits++
+			alertUp += f.up
+			alertDown += f.down
+			alertTotal += alertUp + alertDown
+
+			if f.ts.After(showTS) {
+				displayHits++
+				displayUp += f.up
+				displayDown += f.down
+				displayTotal += displayUp + displayDown
+			}
+		}
+		rows = append(rows, displayRow{
+			Section: section,
+			Hits:    displayHits,
+			Up:      displayUp,
+			Down:    displayDown,
+			Total:   displayTotal,
+		})
+	}
+
+	sort.Slice(rows, func(i, j int) bool {
+		return rows[i].Hits > rows[j].Hits
+	})
+	for _, row := range rows {
+		fmt.Println(row)
+	}
 }
 
-func sectionFromURL(u string) (string, error) {
-	url, err := url.Parse(u)
-	if err != nil {
-		return "", err
+type displayRow struct {
+	Section string
+	Hits    int
+	Up      int
+	Down    int
+	Total   int
+}
+
+func sectionFromHostPath(host, path string) string {
+	if path == "" {
+		path = "/"
 	}
-	if url.Path == "" {
-		url.Path = "/"
-	}
-	path := strings.Join(strings.Split(url.Path, "/")[:2], "/")
-	return fmt.Sprintf("%s://%s%s", url.Scheme, url.Host, path), nil
+	path = strings.Join(strings.Split(path, "/")[:2], "/")
+	return fmt.Sprintf("%s%s", host, path)
 }
 
 func uint16SliceContains(haystack []uint16, needle uint16) bool {
